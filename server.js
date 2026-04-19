934890ebe034647d129da9b23750b2ba0748b236
Jonathan <jonathanfrederickmatias@gmail.com>
2026-04-18 21:58:03 -0300

    corrige servidor railway

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta" + PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Servidor rodando na porta " + PORT);
 });
