72072f23c2a54d67de42b23f586e52eff4b94b77
Jonathan <jonathanfrederickmatias@gmail.com>
2026-04-18 21:21:52 -0300

    ajuste porta railway

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.get("/", (req, res) => {
   res.send("API rodando ✅");
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando");
 });
